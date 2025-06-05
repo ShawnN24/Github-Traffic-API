2a404ad9d2f93981d738c3bbeb87b8d0d5f1f102
Shawn N <shawnneill24@gmail.com>
2025-06-05 16:01:37 -0700

    Update db.py

--- app/db.py
+++ app/db.py
@@ -4,8 +4,7 @@ from app.models import Base, Traffic
 import os
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'traffic.db')}"
+DATABASE_URL = "sqlite:///traffic.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
