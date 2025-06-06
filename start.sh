50fe48bec84702c673033a307765ff87e1ddd989
Shawn N <shawnneill24@gmail.com>
2025-06-05 18:27:44 -0700

    Updates

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading traffic.db from GitHub..."
-curl -o traffic.db https://raw.githubusercontent.com/ShawnN24/Github-Traffic-API/traffic-db-storage/traffic.db
+curl -o traffic.db https://raw.githubusercontent.com/${GITHUB_USERNAME}/Github-Traffic-API/traffic-db-storage/traffic.db
 
 echo "Starting the server..."
 uvicorn app.main:app --host 0.0.0.0 --port 10000
