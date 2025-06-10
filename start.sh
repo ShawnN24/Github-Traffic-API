495f9b43abb14ca2b9f7afeec632711797455829
Shawn N <shawnneill24@gmail.com>
2025-06-09 17:01:32 -0700

    Add metrics.json file to root

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Cloning traffic-db-storage branch..."
-git clone -b traffic-db-storage https://github.com/ShawnN24/Github-Traffic-API.git cloned-repo
-
-echo "Starting the server..."
-uvicorn app.main:app --host 0.0.0.0 --port 10000
